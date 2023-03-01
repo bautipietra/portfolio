94762ff6de5da9275e8470f163b4bd02f262ec8d
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-01 14:57:43 -0300

    structure and config

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    transitionDuration: {
+      DEFAULT: '250ms'
+    },
+    screens: {
+      'xxs': '400px',
+      'xs': '520px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
+  },
+  plugins: [],
+}
