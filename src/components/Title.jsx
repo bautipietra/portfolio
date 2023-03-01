247b4c9f84734b5f96f832e6cc3c2187acd86c27
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-01 15:01:15 -0300

    create title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Title = ({ title }) => {
+  return (
+    <h2 className='text-center font-black text-2xl xxs:text-3xl xs:text-4xl xl:text-5xl text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-sky-700'>{title || 'title'}</h2>
+  )
+}
+
+export default Title
