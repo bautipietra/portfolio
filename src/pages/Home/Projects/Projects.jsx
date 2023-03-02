af6faa5972da42caa2862670cc020c07378318cd
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-02 00:25:50 -0300

    create projects component in home

---
+++ src/pages/Home/Projects/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from '../../../components/Title'
+
+const Projects = () => {
+  return (
+    <div className='grid'>
+      <Title title={'Projects'}></Title>
+    </div>
+  )
+}
+
+export default Projects
