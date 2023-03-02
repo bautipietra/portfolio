32a95b809c1df20c0eac4841c3835904355a2b03
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-02 00:25:01 -0300

    create experience component in home

---
+++ src/pages/Home/Experience/Experience.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import Title from '../../../components/Title'
+import { motion } from 'framer-motion'
+
+const Experience = () => {
+  const { t } = useTranslation(['translation'])
+  return (
+    <div className='grid -mt-20 gap-16 relative'>
+      <motion.div className='absolute top-full shadow-[0px_0px_300px_50px_#0ea5e9]'
+        animate={{ x: [30, -20, 20, -30], y: [30, -20, 20, -30], rotate: [0, 90], scale: [1, 1.2, 1.2, 1] }}
+        transition={{ duration: 10, repeat: Infinity, repeatType: 'reverse' }}
+      ></motion.div>
+      <Title title={t('experience.title')}></Title>
+      <ol
+        className="border-l border-zinc-400 lg:grid grid-cols-3 lg:justify-center lg:gap-16 lg:border-l-0 lg:border-t">
+        <li>
+          <div className="flex-start flex items-center pt-2 lg:block lg:pt-0">
+            <div
+              className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-zinc-400 lg:ml-0 lg:mr-0 lg:-mt-[5px]"></div>
+            <p className="mt-2 text-sm text-zinc-400">
+              {t('experience.nsDate')}
+            </p>
+          </div>
+          <div className="mt-2 ml-4 pb-5 lg:ml-0">
+            <h4 className="mb-1.5 text-xl font-semibold">Next Step <span className='text-sky-400'>, Full Stack Developer</span></h4>
+            <p className="mb-3 text-white"><span className='text-sky-500'>{t('experience.tech')}:</span> React, Redux, Python, Django, MongoDB</p>
+            <p className="mb-3 text-zinc-400 grid gap-2">
+              <span>{t('experience.ns1')}</span>
+              <span>{t('experience.ns2')}</span>
+              <span>{t('experience.ns3')}</span>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div className="flex-start flex items-center pt-2 lg:block lg:pt-0">
+            <div
+              className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-zinc-400 lg:ml-0 lg:mr-0 lg:-mt-[5px]"></div>
+            <p className="mt-2 text-sm text-zinc-400">
+              {t('experience.chDate')}
+            </p>
+          </div>
+          <div className="mt-2 ml-4 pb-5 lg:ml-0">
+            <h4 className="mb-1.5 text-xl font-semibold">Coderhouse <span className='text-sky-400'>, {t('experience.coderhouse')}</span></h4>
+            <p className="mb-3 text-white"><span className='text-sky-500'>{t('experience.tech')}:</span> React, Redux, Python, Django, MongoDB</p>
+            <p className="mb-3 text-zinc-400 grid gap-2">
+              <span>{t('experience.ch1')}</span>
+              <span>{t('experience.ch2')}</span>
+              <span>{t('experience.ch3')}</span>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div className="flex-start flex items-center pt-2 lg:block lg:pt-0">
+            <div
+              className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-zinc-400 lg:ml-0 lg:mr-0 lg:-mt-[5px]"></div>
+            <p className="mt-2 text-sm text-zinc-400">
+              {t('experience.shDate')}
+            </p>
+          </div>
+          <div className="mt-2 ml-4 pb-5 lg:ml-0">
+            <h4 className="mb-1.5 text-xl font-semibold">Soy Henry <span className='text-sky-400'>, {t('experience.soyhenry')}</span></h4>
+            <p className="mb-3 text-white"><span className='text-sky-500'>{t('experience.tech')}:</span> React, Redux, Python, Django, MongoDB</p>
+            <p className="mb-3 text-zinc-400 grid gap-2">
+              <span>{t('experience.sh1')}</span>
+              <span>{t('experience.sh2')}</span>
+              <span>{t('experience.sh3')}</span>
+            </p>
+          </div>
+        </li>
+      </ol>
+    </div>
+  )
+}
+
+export default Experience
