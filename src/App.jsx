449ff2795a0cd974c886bb6fb36256bbf1aea477
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-03 00:05:28 -0300

    add new router to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom"
 import Nav from "./pages/Nav/Nav"
 import Home from "./pages/Home/Home"
 import { Toaster } from 'react-hot-toast';
 import AnimatedCursor from "react-animated-cursor"
 import ScrollToTop from "./components/ScrollToTop";
+import Docs from "./pages/Docs/Docs";
+import { AnimatePresence } from "framer-motion";
+import HTML from "./pages/Docs/HTML/HTML";
+import Tools from "./pages/Tools/Tools";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="grid relative max-w-screen w-full min-h-screen bg-[#090e19] text-white overflow-hidden">
       <AnimatedCursor
@@ -35,10 +40,15 @@ function App() {
           },
         }}
       />
-      <Routes>
-        <Route path="" element={<Home></Home>} />
-        <Route path="*" element={<Home></Home>} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="" element={<Home></Home>} />
+          <Route path="/docs" element={<Docs></Docs>} />
+          <Route path="/docs/html" element={<HTML></HTML>} />
+          <Route path="/tools" element={<Tools></Tools>} />
+          <Route path="*" element={<Home></Home>} />
+        </Routes>
+      </AnimatePresence>
     </div>
   )
 }
