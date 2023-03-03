e6d0397b0a7a39e8dd43879ec45e5f8e793cb5dc
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-03 01:40:35 -0300

    fix background color in tools component

--- src/pages/Tools/Tools.jsx
+++ src/pages/Tools/Tools.jsx
@@ -140,7 +140,7 @@ const Tools = () => {
               exit={{ opacity: 0, scale: 0.9 }}
               transition={{ duration: 0.3, delay: 0.1 * i }}
             >
-              <div className="overflow-hidden  aspect-video bg-red-400 cursor-pointer rounded-xl relative group">
+              <div className="overflow-hidden  aspect-video cursor-pointer rounded-xl relative group">
                 <div className="rounded-xl z-[5] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out cursor-pointer absolute from-black/80 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white flex items-end">
                   <div className="transform-gpu w-full flex justify-between items-center p-4 space-y-3 text-xl group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 pb-10 transform transition duration-300 ease-in-out">
                     <div className="font-bold">{t.name}</div>
