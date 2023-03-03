c0bd10a2d43e4a7ba099c4f91c83ba0299820a6b
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-03 00:09:44 -0300

    add animations to hero component

--- src/pages/Home/Hero/Hero.jsx
+++ src/pages/Home/Hero/Hero.jsx
@@ -28,9 +28,25 @@ const Hero = () => {
   }
 
   return (
-    <div className='flex flex-col xl:flex-row items-center justify-center gap-16 xl:gap-0 xl:justify-between w-full min-h-screen relative mt-40 md:mt-24 xl:mt-0'>
+    <motion.div className='flex flex-col xl:flex-row items-center justify-center gap-16 xl:gap-0 xl:justify-between w-full min-h-screen relative mt-40 md:mt-24 xl:mt-0'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.3 }}
+    >
+
+      {/* Confetti on click at get in touch button */}
       {isExploding && <ConfettiExplosion particleCount={100} className='fixed left-1/2 -translate-x-1/2' />}
+
+      {/* Light */}
+      <motion.div className='absolute mt-[20%] shadow-[0px_0px_300px_50px_#0ea5e9]'
+        animate={{ x: [30, -20, 20, -30], y: [30, -20, 20, -30], rotate: [0, 90], scale: [1, 1.2, 1.2, 1] }}
+        transition={{ duration: 10, repeat: Infinity, repeatType: 'reverse' }}
+      ></motion.div>
+
       <div className='grid items-center gap-4 xl:max-w-2xl text-center xl:text-start'>
+
+        {/* Hero Left side */}
         <h1 className='font-black text-2xl xxs:text-3xl xs:text-4xl sm:text-5xl xl:text-[3.3rem]'>{t('hero.title')} <span className='text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-sky-700'>PietraDev</span><br></br>
           Full Stack Developer
         </h1>
@@ -44,14 +60,12 @@ const Hero = () => {
               'https://drive.google.com/file/d/1Si-6sC49_UTRTeg7uv1sEagCAHCyRPQu/view'
             } target='_blank' className='px-6 py-2 bg-[#112] rounded-xl flex items-center gap-2'>{t('hero.cv')} <AiFillEye></AiFillEye></a>
           </div>
-          <motion.div className='absolute shadow-[0px_0px_300px_50px_#0ea5e9]'
-            animate={{ x: [30, -20, 20, -30], y: [30, -20, 20, -30], rotate: [0, 90], scale: [1, 1.2, 1.2, 1] }}
-            transition={{ duration: 10, repeat: Infinity, repeatType: 'reverse' }}
-          ></motion.div>
         </div>
       </div>
+
+      {/* Hero right */}
       <Code></Code>
-    </div>
+    </motion.div>
   )
 }
 
