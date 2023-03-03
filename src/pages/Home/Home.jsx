1b9f4e8db81d9895d86233a8e6a95ef7ac029378
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-03 00:08:26 -0300

    add animation in home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,17 +8,26 @@ import Projects from './Projects/Projects'
 
 const Home = () => {
   return (
-    <main className='max-w-7xl m-auto px-2 xs:px-8 w-full grid gap-24'>
-      <motion.div className='absolute  left-full -translate-x-1/2 top-[100vh]  shadow-[0px_0px_300px_50px_#0ea5e9]'
-        animate={{ x: [30, -20, 20, -30], y: [30, -20, 20, -30], rotate: [0, 90], scale: [1, 1.2, 1.2, 1] }}
-        transition={{ duration: 10, repeat: Infinity, repeatType: 'reverse' }}
-      ></motion.div>
+    <motion.main className='max-w-7xl m-auto px-2 xs:px-8 w-full grid gap-24'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.3 }}
+    >
+      {/* Components */}
       <Hero></Hero>
       <Experience></Experience>
       <Projects></Projects>
       <Blog></Blog>
       <Contact></Contact>
-    </main>
+
+      {/* Light */}
+      <motion.div className='absolute  left-full -translate-x-1/2 top-[100vh]  shadow-[0px_0px_300px_50px_#0ea5e9]'
+        animate={{ x: [30, -20, 20, -30], y: [30, -20, 20, -30], rotate: [0, 90], scale: [1, 1.2, 1.2, 1] }}
+        transition={{ duration: 10, repeat: Infinity, repeatType: 'reverse' }}
+      ></motion.div>
+
+    </motion.main>
   )
 }
 
