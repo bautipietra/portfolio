b531909fd216348b3cfe7528d1c6709ccf348fb1
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-03 00:10:21 -0300

    fix links hover animation in nav

--- src/pages/Nav/Links.jsx
+++ src/pages/Nav/Links.jsx
@@ -4,15 +4,18 @@ import { Link } from 'react-router-dom'
 const Links = ({ path }) => {
   return (
     <div className='grid-cols-4 relative text-center items-center group hidden md:grid'>
+      {/* Links */}
       <span id='home' className='font-semibold text-lg w-full'><Link to={'/'} className='w-full px-4 py-2'>Home</Link></span>
       <span id='blog' className='font-semibold text-lg w-full'><Link to={'/blog'} className='w-full px-4 py-2'>Blog</Link></span>
       <span id='docs' className='font-semibold text-lg w-full'><Link to={'/docs'} className='w-full px-4 py-2'>Docs</Link></span>
       <span id='tools' className='font-semibold text-lg w-full'><Link to={'/tools'} className='w-full px-4 py-2'>Tools</Link></span>
+
+      {/* Background links hover animation */}
       <div id='navHover' className={`absolute w-full h-[150%] rounded-xl -z-10 bg-gradient-to-r  from-sky-500 to-sky-700 col-start-1 col-span-1 transition-all 
     ${path == '/' && 'left-0'}
-    ${path == '/blog' && 'left-[100%]'}
-    ${path == '/docs' && 'left-[200%]'}
-    ${path == '/tools' && 'left-[300%]'}
+    ${path.includes('/blog') && 'left-[100%]'}
+    ${path.includes('/docs') && 'left-[200%]'}
+    ${path.includes('/tools') && 'left-[300%]'}
     `}></div>
     </div>
   )
--- src/pages/Nav/MenuOpen.jsx
+++ src/pages/Nav/MenuOpen.jsx
@@ -13,15 +13,19 @@ const MenuOpen = ({ isOpen, closeMenu }) => {
         transition={{ duration: 0.3 }}
       >
         <div className='grid-cols-4 relative text-center items-center group grid z-20'>
+
+          {/* Links */}
           <span onClick={closeMenu} id='home' className='font-semibold text-lg w-full'><Link to={'/'} className='w-full px-4 py-2'>Home</Link></span>
           <span onClick={closeMenu} id='blog' className='font-semibold text-lg w-full'><Link to={'/blog'} className='w-full px-4 py-2'>Blog</Link></span>
           <span onClick={closeMenu} id='docs' className='font-semibold text-lg w-full'><Link to={'/docs'} className='w-full px-4 py-2'>Docs</Link></span>
           <span onClick={closeMenu} id='tools' className='font-semibold text-lg w-full'><Link to={'/tools'} className='w-full px-4 py-2'>Tools</Link></span>
+
+          {/* Background links hover animation */}
           <div id='navHover' className={`absolute w-full h-[150%] rounded-xl -z-10 bg-gradient-to-r  from-sky-500 to-sky-700 col-start-1 col-span-1 transition-all 
-  ${path == '/' && 'left-0'}
-  ${path == '/blog' && 'left-[100%]'}
-  ${path == '/docs' && 'left-[200%]'}
-  ${path == '/tools' && 'left-[300%]'}
+    ${path == '/' && 'left-0'}
+    ${path.includes('/blog') && 'left-[100%]'}
+    ${path.includes('/docs') && 'left-[200%]'}
+    ${path.includes('/tools') && 'left-[300%]'}
   `}></div>
         </div>
       </motion.div>}
