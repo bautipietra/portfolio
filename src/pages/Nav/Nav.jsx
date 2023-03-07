153c3d1aa9c4f16c79f076aac6c87c4d57cc551f
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-07 11:40:40 -0300

    update logo

--- src/pages/Nav/Nav.jsx
+++ src/pages/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../../assets/logo.png'
+import Logo from '../../assets/logo.webp'
 import { Link, useHref } from 'react-router-dom'
 import Language from './Language'
 import Links from './Links'
@@ -11,7 +11,7 @@ const Nav = () => {
   return (
     <nav className='max-w-7xl w-full p-8 flex justify-between items-center absolute top-0 left-1/2 -translate-x-1/2 z-10'>
       <Link to={'/'} className='z-20'>
-        <img src={Logo} alt="pietradev logo" className='object-cover max-h-12' />
+        <img src={Logo} alt="pietradev logo" className='object-cover max-h-10' />
       </Link>
       <Links path={path}></Links>
       <Language></Language>
