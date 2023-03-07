179a84860ecce7b88eade3819b8ed98f0e7e93d4
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-07 11:41:04 -0300

    fix custom cursor in scroll to top component

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -16,21 +16,23 @@ const ScrollToTop = () => {
     }, 200);
   }
   return (
-    <AnimatePresence>
+    <div>
+      <AnimatePresence>
+        {
+          scroll > '10' ?
+            <motion.div onClick={scrollHandler} className='fixed right-8 bottom-8 bg-gradient-to-r  from-sky-500 to-sky-700 ring-0 rounded-full p-2 cursorClick z-[5]'
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0 }}
+              transition={{ duration: 0.3, ease: 'easeInOut' }}
+            >
+              <IoIosArrowUp size={'25px'}></IoIosArrowUp>
+            </motion.div> :
+            null
+        }
+      </AnimatePresence>
       <CustomCursor></CustomCursor>
-      {
-        scroll > '10' ?
-          <motion.div onClick={scrollHandler} className='fixed right-8 bottom-8 bg-gradient-to-r  from-sky-500 to-sky-700 ring-0 rounded-full p-2 cursorClick z-[5]'
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0 }}
-            transition={{ duration: 0.3, ease: 'easeInOut' }}
-          >
-            <IoIosArrowUp size={'25px'}></IoIosArrowUp>
-          </motion.div> :
-          null
-      }
-    </AnimatePresence>
+    </div>
   )
 }
 
