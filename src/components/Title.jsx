6cf3c2d8c3659dbbbb9edd8d3efb8555c17e9a50
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-10 20:46:48 -0300

    fix height in title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Title = ({ title }) => {
   return (
-    <h2 className='text-center font-black text-2xl xxs:text-3xl xs:text-4xl xl:text-5xl text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-sky-700'>{title || 'title'}</h2>
+    <h2 className='text-center font-black text-2xl xxs:text-3xl xs:text-4xl xl:text-5xl text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-sky-700 h-[115%]'>{title || 'title'}</h2>
   )
 }
 
