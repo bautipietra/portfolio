dc685d29634c72ff50fffb54a6ad4cde969d566c
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-10 20:47:15 -0300

    add light to tools component

--- src/pages/Tools/Tools.jsx
+++ src/pages/Tools/Tools.jsx
@@ -125,6 +125,20 @@ const Tools = () => {
       {/* Custom cursor */}
       <CustomCursor></CustomCursor>
 
+      {/* Lights */}
+      <motion.div className='absolute  left-full -translate-x-1/2 top-[100vh]  shadow-[0px_0px_300px_50px_#0ea5e9]'
+        animate={{ x: [30, -20, 20, -30], y: [30, -20, 20, -30], rotate: [0, 90], scale: [1, 1.2, 1.2, 1] }}
+        transition={{ duration: 10, repeat: Infinity, repeatType: 'reverse' }}
+      ></motion.div>
+      <motion.div className='absolute  -translate-x-1/2 top-[40vh]  shadow-[0px_0px_300px_50px_#0ea5e9]'
+        animate={{ x: [30, -20, 20, -30], y: [30, -20, 20, -30], rotate: [0, 90], scale: [1, 1.2, 1.2, 1] }}
+        transition={{ duration: 10, repeat: Infinity, repeatType: 'reverse' }}
+      ></motion.div>
+      <motion.div className='absolute  -translate-x-1/2 top-full  shadow-[0px_0px_300px_50px_#0ea5e9]'
+        animate={{ x: [30, -20, 20, -30], y: [30, -20, 20, -30], rotate: [0, 90], scale: [1, 1.2, 1.2, 1] }}
+        transition={{ duration: 10, repeat: Infinity, repeatType: 'reverse' }}
+      ></motion.div>
+
       {/* Categories */}
       <div className='flex gap-2 justify-center items-center flex-wrap'>
         {categories.map((c, i) => (
