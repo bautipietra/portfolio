d49b0c1b870fe816d10dd864d52a7c0d573725ee
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-12 17:29:58 -0300

    update blog style in home

--- src/pages/Home/Blog/Blog.jsx
+++ src/pages/Home/Blog/Blog.jsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion'
 import Title from '../../../components/Title'
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
+import BottomContainer from '../../../components/BottomContainer'
 
 const Blog = () => {
   const { t } = useTranslation('translation')
@@ -18,21 +19,24 @@ const Blog = () => {
           animate={{ x: [30, -20, 20, -30], y: [30, -20, 20, -30], rotate: [0, 90], scale: [1, 1.2, 1.2, 1] }}
           transition={{ duration: 10, repeat: Infinity, repeatType: 'reverse' }}
         ></motion.div>
-        <Link className='p-8 bg-gradient-to-r  from-slate-900 to-slate-800 ring-0 rounded-xl grid gap-4 group'>
+        <Link className='p-8 bg-gradient-to-r  from-slate-900 to-slate-800 ring-0 rounded-xl grid gap-4 group relative overflow-hidden'>
+          <BottomContainer></BottomContainer>
           <h3 className='font-semibold text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
           <div className='flex justify-between text-zinc-400'>
             <span>10/30/2003</span>
             <span className='flex gap-2 items-center group-hover:text-sky-500 transition-colors'>{t('blog.read')} <BsArrowRight size={'20px'}></BsArrowRight></span>
           </div>
         </Link>
-        <Link className='p-8 bg-gradient-to-r  from-slate-900 to-slate-800 ring-0 rounded-xl grid gap-4 group'>
+        <Link className='p-8 bg-gradient-to-r  from-slate-900 to-slate-800 ring-0 rounded-xl grid gap-4 group relative overflow-hidden'>
+          <BottomContainer></BottomContainer>
           <h3 className='font-semibold text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
           <div className='flex justify-between text-zinc-400'>
             <span>10/30/2003</span>
             <span className='flex gap-2 items-center group-hover:text-sky-500 transition-colors'>{t('blog.read')} <BsArrowRight size={'20px'}></BsArrowRight></span>
           </div>
         </Link>
-        <Link className='p-8 bg-gradient-to-r  from-slate-900 to-slate-800 ring-0 rounded-xl grid gap-4 group'>
+        <Link className='p-8 bg-gradient-to-r  from-slate-900 to-slate-800 ring-0 rounded-xl grid gap-4 group relative overflow-hidden'>
+          <BottomContainer></BottomContainer>
           <h3 className='font-semibold text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
           <div className='flex justify-between text-zinc-400'>
             <span>10/30/2003</span>
