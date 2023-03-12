f02ee6590a6fcb37e916839b431d488c105104bc
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-12 17:29:44 -0300

    create bottomContainer component

---
+++ src/components/BottomContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BottomContainer = () => {
+  return (
+    <span className="absolute inset-x-0 bottom-0 h-1 bg-gradient-to-r from-sky-500 to-sky-700"></span>
+  )
+}
+
+export default BottomContainer
