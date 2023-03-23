03021862006e3f1d86aa3111a6470a7aa13a280b
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-23 20:44:18 -0300

    create articles data component

---
+++ src/pages/Blog/Articles.jsx
@@ -0,0 +1,78 @@
+/* 
+
+  {
+    thumbnail: 'imagen.jpg',
+    url: 'titulo',
+    title: 'titulo',
+    description: 'descripción',
+    content: ``,
+    date: '31/12/2023'
+  }
+
+*/
+
+const articles = [
+  {
+    thumbnail: 'https://i.imgur.com/dN7sosH.jpg',
+    url: 'ruta+de+prendizaje+para+ser+un+programador+full+stack',
+    title: 'Ruta de aprendizaje para ser un programador full stack',
+    description:
+      'Un roadmap de aprendizaje puede ayudarte a mantener el rumbo y a no perderte en el camino. Te proporciona una guía clara y estructurada para aprender las habilidades y tecnologías necesarias para convertirte en un experto en el desarrollo web.',
+    content: `
+    <p>Como desarrollador full-stack, hay una gran cantidad de habilidades y tecnologías que necesitas conocer. Es fácil sentirse abrumado por la cantidad de información disponible, especialmente si eres nuevo en el campo.</p>
+	<p>Un roadmap de aprendizaje puede ayudarte a mantener el rumbo y a no perderte en el camino. Te proporciona una guía clara y estructurada para aprender las habilidades y tecnologías necesarias para convertirte en un experto en el desarrollo web.</p>
+	<p>Ahora, veamos cómo puedes crear un roadmap efectivo y estructurado para tu aprendizaje como desarrollador full-stack.</p>
+	<h2>Front-End</h2>
+	<img src="https://bs-uploads.toptal.io/blackfish-uploads/components/seo/content/og_image_file/og_image/777499/op-Ten-Front-End-Design-Rules-For-Developers_Luke-Social-33a3a7c9b759fdaa22973906070f8065.png" alt="Logo de HTML, CSS y JavaScript">
+	<p>El front-end es la parte del desarrollo web que se encarga de la interfaz de usuario y la experiencia del usuario. Estas son las habilidades y tecnologías que necesitas conocer:</p>
+	<ul>
+		<li>HTML</li>
+		<li>CSS</li>
+		<li>JavaScript</li>
+		<li>Frameworks de JavaScript como React, Vue, o Angular</li>
+		<li>Responsive Design</li>
+		<li>Accesibilidad</li>
+	</ul>
+	<h2>Back-End</h2>
+	<img src="https://www.azulschool.net/wp-content/uploads/2020/05/Backend-Developer-1024x768.png" alt="Logo de Node.js, Python y Ruby">
+	<p>El back-end es la parte del desarrollo web que se encarga de la lógica del servidor y el almacenamiento de datos. Estas son las habilidades y tecnologías que necesitas conocer:</p>
+	<ul>
+		<li>Bases de datos relacionales (por ejemplo PostgreSQL)</li>
+		<li>Bases de datos NoSQL (por ejemplo MongoDB)</li>
+		<li>APIs (Application Programming Interfaces)</li>
+	</ul>
+		<p>También deberías elegir un lenguaje para el servidor, algunas posibles opciones son:</p>
+	<ul>
+		<li>Javascript con Node.js</li>
+		<li>Python con Django o Flask</li>
+		<li>Ruby con Ruby on Rails</li>
+	</ul>
+	<h2>Patrones de Diseño</h2>
+	<img src="https://community.arm.com/cfs-file/__key/communityserver-blogs-components-weblogfiles/00-00-00-19-96/Programming-code-script.jpg" alt="Imagen de patrones de diseño">
+	<p>Los patrones de diseño son soluciones probadas para problemas comunes de diseño de software. Estos son algunos ejemplos de patrones de diseño:</p>
+	<ul>
+		<li>MVC (Model-View-Controller)</li>
+		<li>MVVM (Model-View-ViewModel)</li>
+		<li>Singleton</li>
+		<li>Factory</li>
+		<li>Dependency Injection</li>
+	</ul>
+      <h2>Control de Versiones con Git</h2>
+<img src="https://kinsta.com/es/wp-content/uploads/sites/8/2023/02/github-pages.jpg" alt="Logo de Git">
+<p>Git es un sistema de control de versiones que te permite colaborar con otros desarrolladores en un proyecto y mantener un historial de cambios en el código. Estas son las habilidades y tecnologías que necesitas conocer:</p>
+<ul>
+	<li>Conceptos básicos de Git (como commits, ramas, fusiones y conflictos)</li>
+	<li>Plataformas de alojamiento de código (como GitHub o GitLab)</li>
+	<li>Flujo de trabajo de Git (como GitFlow)</li>
+</ul>
+  <h2>Conclusión</h2>
+  <p>Espero que este artículo te haya sido útil para crear tu propio roadmap de aprendizaje como desarrollador full-stack. Recuerda que no hay una sola ruta de aprendizaje correcta y que siempre hay más de una forma de resolver un problema.</p>
+	<p>Además, ten en cuenta que no hay tecnologías "mejores" que otras. Lo importante es utilizar las herramientas adecuadas para el trabajo en cuestión y estar dispuesto a aprender nuevas tecnologías y habilidades en el camino.</p>
+	<p>En resumen, ser un desarrollador full-stack requiere un esfuerzo continuo de aprendizaje y crecimiento. Con un roadmap bien estructurado y una mentalidad de aprendizaje continuo, puedes alcanzar tus metas y convertirte en un experto en el desarrollo web.</p>
+	<p>¡Buena suerte en tu camino como desarrollador full-stack!</p>
+    `,
+    date: '23/03/2023'
+  }
+];
+
+export default articles;
