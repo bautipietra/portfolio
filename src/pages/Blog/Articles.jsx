464563fab2cbba3560c60b12c3bc274daaed8dc6
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-24 21:35:27 -0300

    add new articles to blog

--- src/pages/Blog/Articles.jsx
+++ src/pages/Blog/Articles.jsx
@@ -4,10 +4,10 @@
     thumbnail: 'imagen.jpg',
     url: 'titulo',
     title: 'titulo',
+	  date: '31/12/2023',
+		tags: ['#tag'],
     description: 'descripción',
-    content: ``,
-    date: '31/12/2023',
-		tags: ['#tag']
+    content: ``
   }
 
 */
@@ -15,10 +15,21 @@
 const articles = [
   {
     thumbnail: 'https://i.imgur.com/dN7sosH.jpg',
-    url: 'ruta+de+prendizaje+para+ser+un+programador+full+stack',
+    url: 'ruta+de+aprendizaje+para+ser+un+programador+full+stack',
     title: 'Ruta de aprendizaje para ser un programador full stack',
     description:
       'Un roadmap de aprendizaje puede ayudarte a mantener el rumbo y a no perderte en el camino. Te proporciona una guía clara y estructurada para aprender las habilidades y tecnologías necesarias para convertirte en un experto en el desarrollo web.',
+    date: '23/03/2023',
+    tags: [
+      '#DesarrolloWeb',
+      '#DesarrolloFullStack',
+      '#Roadmap',
+      '#Tecnologías',
+      '#Skills',
+      '#PatronesDeDiseño',
+      '#FrontEnd',
+      '#BackEnd'
+    ],
     content: `
     <p>Como desarrollador full-stack, hay una gran cantidad de habilidades y tecnologías que necesitas conocer. Es fácil sentirse abrumado por la cantidad de información disponible, especialmente si eres nuevo en el campo.</p>
 	<p>Un roadmap de aprendizaje puede ayudarte a mantener el rumbo y a no perderte en el camino. Te proporciona una guía clara y estructurada para aprender las habilidades y tecnologías necesarias para convertirte en un experto en el desarrollo web.</p>
@@ -71,18 +82,112 @@ const articles = [
 	<p>Además, ten en cuenta que no hay tecnologías "mejores" que otras. Lo importante es utilizar las herramientas adecuadas para el trabajo en cuestión y estar dispuesto a aprender nuevas tecnologías y habilidades en el camino.</p>
 	<p>En resumen, ser un desarrollador full-stack requiere un esfuerzo continuo de aprendizaje y crecimiento. Con un roadmap bien estructurado y una mentalidad de aprendizaje continuo, puedes alcanzar tus metas y convertirte en un experto en el desarrollo web.</p>
 	<p>¡Buena suerte en tu camino como desarrollador full-stack!</p>
-    `,
-    date: '23/03/2023',
+    `
+  },
+
+  {
+    thumbnail: 'https://i.imgur.com/MR5xdyM.jpg',
+    url: 'Extensiones+de+Visual+Studio+Code+que+todo+desarrollador+debería+conocer',
+    title:
+      'Extensiones de Visual Studio Code que todo desarrollador debería conocer',
+    description:
+      'Una de las razones por las que VSC es tan popular es porque es altamente personalizable gracias a sus numerosas extensiones. Estas pueden mejorar la productividad del desarrollador, proporcionando herramientas adicionales y características útiles que no se encuentran en el editor de forma nativa.',
+    date: '11/03/2023',
     tags: [
-      '#DesarrolloWeb',
-      '#DesarrolloFullStack',
-      '#Roadmap',
-      '#Tecnologías',
-      '#Skills',
-      '#PatronesDeDiseño',
-      '#FrontEnd',
-      '#BackEnd'
-    ]
+      '#vscode',
+      '#extensiones',
+      '#desarrolloweb',
+      '#fullstack',
+      '#snippet',
+      '#formateadordecodigo',
+      '#temas',
+      '#liveshare',
+      '#pathintellisense',
+      '#thunderclient',
+      '#productividad',
+      '#programación',
+      '#tecnología',
+      '#desarrolladores'
+    ],
+    content: `
+		<p>Visual Studio Code es uno de los editores de código más populares y utilizados por los desarrolladores de todo el mundo. Una de las razones por las que es tan popular es porque es altamente personalizable gracias a sus numerosas extensiones. Las extensiones pueden mejorar la productividad del desarrollador, proporcionando herramientas adicionales y características útiles que no se encuentran en el editor de forma nativa.</p>
+	<p>En este artículo, te mostrare extensiones gratuitas y pueden ayudarte a mejorar tu flujo de trabajo, hacer tareas cotidianas más eficientes y mejorar la calidad de tu código.</p>
+<h2>Live Server</h2>
+<p>Esta extensión es perfecta para aquellos desarrolladores que trabajan en aplicaciones web. Con esta herramienta, puedes crear un servidor web local en tiempo real y ver los cambios que haces en tu código reflejados de inmediato en el navegador.</p>
+<img src="https://debug.to/?qa=blob&qa_blobid=4790030382953512245" alt="Live Server">
+<h2>GitLens</h2>
+<p>GitLens es una extensión que facilita la visualización y el seguimiento de los cambios realizados en tu repositorio de Git. Con esta herramienta, puedes ver quién modificó cada línea de código, cuándo se realizó la modificación y qué cambios se hicieron.</p>
+<h2>Extensiones para ver y gestionar errores</h2>
+<ol>
+	<li><strong>Error Lens</strong> - Muestra errores y advertencias en el código, resaltando el texto que los causa y proporcionando información adicional.</li>
+	<li><strong>ESLint</strong> - Es una herramienta que ayuda a los desarrolladores a detectar errores en el código JavaScript y mantener un estilo de codificación consistente. Esta extensión es compatible con varios frameworks de JavaScript, como React, Vue y Angular.</li>
+</ol>
+<h2>Extensiones de snippets</h2>
+<ol>
+	<li><strong>Code Snippets</strong> - Permite a los desarrolladores crear, editar y administrar snippets de código personalizados para aumentar la productividad.</li>
+	<li><strong>JavaScript (ES6) code snippets</strong> - Proporciona snippets de código útiles para JavaScript y ES6, lo que permite a los desarrolladores escribir código más rápido y reducir los errores.</li>
+</ol>
+<h2>Extensiones de formateo de código</h2>
+<ol>
+	<li><strong>Prettier - Code formatter</strong> - Formatea automáticamente el código de acuerdo con las reglas de estilo configuradas, lo que ahorra tiempo y ayuda a mantener un estilo coherente en todo el proyecto.</li>
+	<li><strong>Beautify</strong> - Formatea el código de diferentes lenguajes de programación, incluyendo HTML, CSS, JavaScript y JSON.</li>
+	<br/>
+	<img src="https://i.stack.imgur.com/cT96W.png" alt="prettier vscode">
+</ol>
+<h2>Extensiones de colaboración</h2>
+<ol>
+	<li><strong>Live Share</strong> - Permite a los desarrolladores colaborar en tiempo real en el mismo código, lo que hace que el proceso de codificación sea más eficiente y productivo.</li>
+</ol>
+<h2>Extensiones de mejora de navegación y búsqueda</h2>
+<ol>
+	<li><strong>Path Intellisense</strong> - Ayuda a los desarrolladores a escribir rutas de archivo y directorio de manera más eficiente, proporcionando autocompletado inteligente.</li>
+</ol>
+<h2>Extensiones de temas</h2>
+<ol>
+	<li><strong>Material Theme</strong> - Un tema elegante y moderno que usa colores suaves y atractivos.</li>
+	<li><strong>One Dark Pro</strong> - Un tema oscuro que combina tonos suaves de azul y púrpura, y es ideal para trabajar en entornos de poca luz.</li>
+</ol>
+<br/>
+<img src="https://i.imgur.com/9ff62aM.png" alt="One dark pro theme vscode">
+<h2>Extensiones de prueba y depuración</h2>
+<ol>
+	<li><strong>Thunder Client</strong> - Una extensión para realizar pruebas de API RESTful con una interfaz de usuario fácil de usar y características avanzadas como entornos de prueba y variables de entorno.</li>
+</ol>
+<h2>Conclusión</h2>
+<p>Estas son solo algunas de las muchas extensiones útiles que están disponibles en el mercado. Al usar estas herramientas, los desarrolladores pueden mejorar su productividad y la calidad de su trabajo. Recuerda que cada desarrollador tiene su propio flujo de trabajo y preferencias personales, por lo que siempre es importante explorar diferentes opciones para encontrar las herramientas que mejor se adapten a tus necesidades.</p>
+<p>En resumen, las extensiones de Visual Studio Code son una gran manera de personalizar y mejorar tu experiencia de programación. Desde resaltar la sintaxis hasta administrar el control de versiones, hay muchas herramientas disponibles para ayudarte a ser más productivo y eficiente. Espero que esta lista de 10 extensiones útiles te ayude a mejorar tu flujo de trabajo y a hacer tareas cotidianas más eficientes.</p>
+		`
+  },
+
+  {
+    thumbnail: 'https://i.imgur.com/6m1OIzX.png',
+    url: 'Las+mejores+páginas+para+practicar+programación',
+    title: 'Las mejores páginas para practicar programación',
+    date: '26/02/2023',
+    tags: [
+      'programación',
+      'desarrollo web',
+      'desarrollo de software',
+      'hackerrank',
+      'codewars',
+      'leetcode',
+      'programación en línea',
+      'práctica de programación'
+    ],
+    description:
+      'Como desarrollador, sabes que la programación no se trata solo de aprender lenguajes, sino también de practicar la lógica y mejorar tus habilidades en la solución de problemas. Para hacer esto, es importante encontrar oportunidades para practicar y aplicar lo que has aprendido.',
+    content: `<p>Como desarrollador, sabes que la programación no se trata solo de aprender lenguajes, sino también de practicar la lógica y mejorar tus habilidades en la solución de problemas. Para hacer esto, es importante encontrar oportunidades para practicar y aplicar lo que has aprendido.</p>
+<h2>1. HackerRank</h2>
+<p>HackerRank es una de las plataformas más populares para mejorar tus habilidades de programación. Ofrece una variedad de desafíos y problemas de programación en más de 35 lenguajes de programación diferentes. Puedes encontrar problemas de dificultad fácil, intermedia y avanzada, y también participar en competencias de programación.</p>
+<img src="https://hrcdn.net/og/default.jpg" alt="HackerRank">
+<h2>2. LeetCode</h2>
+<p>LeetCode es otra plataforma popular que ofrece problemas de programación para mejorar tus habilidades. Al igual que HackerRank, LeetCode ofrece desafíos y competencias de programación en una variedad de lenguajes de programación. Además, puedes participar en entrevistas simuladas y recibir comentarios en tus soluciones de problemas.</p>
+<img src="https://www.justalternativeto.com/wp-content/uploads/2021/07/LeetCode.jpg" alt="LeetCode">
+<h2>3. CodeWars</h2>
+<p>CodeWars es una plataforma de programación que te desafía a resolver problemas cada vez más complejos. Con cada problema resuelto, ganas puntos y asciendes en la clasificación de CodeWars. CodeWars ofrece desafíos en varios lenguajes de programación, incluyendo JavaScript, Ruby, Python, Java y C#. </p>
+<img src="https://uploads-ssl.webflow.com/62e3ee10882dc50bcae8d07a/634816d46fc4a32b2a583416_codewars-og-image.png" alt="CodeWars">
+<h2>Conclusión</h2>
+<p>En conclusión, practicar programación es una excelente manera de mejorar tus habilidades y mantenerse actualizado con las últimas tendencias y tecnologías. Ya sea que seas un principiante o un programador experimentado, estas páginas te ayudarán a mejorar tu lógica de programación y a resolver problemas de manera más efectiva. No olvides que la práctica hace al maestro, así que no dudes en dedicar tiempo regularmente a estas páginas para seguir mejorando tus habilidades de programación.</p>`
   }
 ];
 
